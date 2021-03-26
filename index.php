0dfc8a6da136f39452eeabce74ab50bf48acbe81
k1574 <k1574@yandex.ru>
2021-03-27 01:30:37 +0500

    Init.

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<!-- В задании не было указано, как страница
+     должна отображаться при разных размерах,
+     так что для соответсвия
+     будет использовано CSS свойство "display: flex". -->
+<html lang="ru">
+<head>
+
+<meta http-eqiuv="content-type" content="text/html" charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="shortcut icon" href="wbp_logo.png" type="image/png">
+<title>Калькулятор - World Bank Publications</title>
+  
+</head>
+<body>
+
+<header>
+
+  <div class="logo_container">
+    <img src="wbp_logo.png">
+    <div class="text">
+      <div class="uppercase">world bank</div>
+      <div class="capitalize">publications</div>
+    </div>
+  </div>
+
+</header>
+
+<nav>
+  <ul>
+  
+  <li><a href="#">Кредитные карты</a></li>
+  <li class="choosen"><a href="#">Вклады</a></li>
+  <li><a href="#">Дебетовая карта</a></li>
+  <li><a href="#">Страхование</a></li>
+  <li><a href="#">Друзья</a></li>
+  <li><a href="#">Интернет-банк</a></li>
+    
+  </ul>
+</nav>
+
+</body>
+<html>
