db667dbd4f30f4c6c4fc25bb8c7a2d8d5bd74ff3
k1574 <k1574@yandex.ru>
2021-03-28 23:40:12 +0500

    Footer is done.

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <!-- В задании не было указано, как страница
      должна отображаться при разных размерах,
      так что для соответсвия
-     будет использовано CSS свойство "display: flex". -->
+     будет использовано CSS свойство "display: flex"
+     и в некоторых случаях "display: table". -->
 <html lang="ru">
 <head>
 
@@ -16,28 +17,40 @@
 
 <header>
 
-  <div class="logo_container">
+  <div class="logo">
     <img src="wbp_logo.png">
     <div class="text">
       <div class="uppercase">world bank</div>
       <div class="capitalize">publications</div>
     </div>
   </div>
+  
+  <div class="contacts">
+    <a class="phone-number" href="tel:88001005005">8-800-100-5005</a>
+    <a class="phone-number" href="tel:+73452522000">+7(3452)522-000</a>
+  </div>
 
 </header>
 
 <nav>
-  <ul>
-  
-  <li><a href="#">Кредитные карты</a></li>
-  <li class="choosen"><a href="#">Вклады</a></li>
-  <li><a href="#">Дебетовая карта</a></li>
-  <li><a href="#">Страхование</a></li>
-  <li><a href="#">Друзья</a></li>
-  <li><a href="#">Интернет-банк</a></li>
-    
-  </ul>
+
+  <a href="#">Кредитные карты</a>
+  <a class="choosen" href="#">Вклады</a>
+  <a href="#">Дебетовая карта</a>
+  <a href="#">Страхование</a>
+  <a href="#">Друзья</a>
+  <a href="#">Интернет-банк</a>
+
 </nav>
+
+<footer>
+  <a href="#">Кредитные карты</a>
+  <a href="#">Вклады</a>
+  <a href="#">Дебетовая карта</a>
+  <a href="#">Страхование</a>
+  <a href="#">Друзья</a>
+  <a href="#">Интернет-банк</a>
+</footer>
 
 </body>
 <html>
